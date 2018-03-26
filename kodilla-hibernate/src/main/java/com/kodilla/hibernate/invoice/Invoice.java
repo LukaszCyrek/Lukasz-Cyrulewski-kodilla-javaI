ae08ff93338870513aa8a7f877383972ea098959
LukaszCyrek <lukaszcyrek@o2.pl>
2018-03-26 20:30:50 +0200

    First Commit

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/invoice/Invoice.java
@@ -0,0 +1,52 @@
+package com.kodilla.hibernate.invoice;
+
+import com.sun.javafx.beans.IDProperty;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "INVOICE")
+public class Invoice {
+    private int id;
+    private String number;
+    private List<Item> items = new ArrayList<>();
+
+    public Invoice(String number, List<Item> items) {
+        this.number = number;
+        this.items = items;
+    }
+
+    @Id
+    @NotNull
+    @GeneratedValue
+    @Column(name = "ID", unique = true)
+    public int getId() {
+        return id;
+    }
+
+    @Column(name = "NUMBER")
+    public String getNumber() {
+        return number;
+    }
+
+    @Column(name = "ITEMS")
+    public List<Item> getItems() {
+        return items;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    private void setNumber(String number) {
+        this.number = number;
+    }
+
+    private void setItems(List<Item> items) {
+        this.items = items;
+    }
+}
+
---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/invoice/Item.java
@@ -0,0 +1,90 @@
+package com.kodilla.hibernate.invoice;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "ITEM")
+public class Item {
+    private int id;
+    private Product product;
+    private BigDecimal price;
+    private int quantity;
+    private BigDecimal value;
+    private List<Product> products = new ArrayList<>();
+
+    public Item() {
+    }
+
+    public Item(BigDecimal price, int quantity, BigDecimal value) {
+        this.price = price;
+        this.quantity = quantity;
+        this.value = value;
+    }
+
+    @Id
+    @NotNull
+    @GeneratedValue
+    @Column(name = "ID", unique = true)
+    public int getId() {
+        return id;
+    }
+
+
+    public Product getProduct() {
+        return product;
+    }
+
+    @Column(name = "PRICE")
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    @Column(name = "QUANTITY")
+    public int getQuantity() {
+        return quantity;
+    }
+
+    @Column(name = "VALUE")
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    private void setProduct(Product product) {
+        this.product = product;
+    }
+
+    private void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    private void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    private void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    @OneToMany(
+            targetEntity = Product.class,
+            mappedBy = "Product",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    @JoinColumn(name = "Product")
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    private void setProducts(List<Product> products) {
+        this.products = products;
+    }
+}
